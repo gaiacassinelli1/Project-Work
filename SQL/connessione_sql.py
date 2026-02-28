7f7380a23d90fa48dbc6e86a8259c7777a1bbdaf
gaiacassinelli1 <cassinelligaia@gmail.com>
2026-02-28 10:13:19 +0100

    modifica connessione

--- SQL/connessione_sql.py
+++ SQL/connessione_sql.py
@@ -1,38 +1,6 @@
-'''
-import os
-from dotenv import load_dotenv
+# connessione file mysql
 import mysql.connector
-load_dotenv()
-conn = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME")
-)'''
-
 import os
-from dotenv import load_dotenv
-import mysql.connector
-
-load_dotenv()
-
-conn = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME"),
-    auth_plugin='mysql_native_password'  # Add this line
-)
-
-print("Connection successful!")
-cursor = conn.cursor()
-cursor.execute("SELECT DATABASE();")
-print("Connected to database:", cursor.fetchone()[0])
-cursor.close()
-conn.close()
-
-# FUNZIONE RIUTILIZZABILE PER MYSQL
-# ========================================
 
 def get_mysql_connection():
     """
